5d8f35e69717fb12fdc4522d79beb29682b04872
Tim Palac <tim.palac@gmail.com>
2019-01-23 15:25:09 -0600

    Adding 5 day forecast, updating styles

--- controllers/resultsController.js
+++ controllers/resultsController.js
@@ -3,6 +3,7 @@
 	.controller("resultsController",['$scope', '$location', '$routeParams', 'customSearch', function($scope, $location, $routeParams, customSearch){
 		$scope.searchTerm = $routeParams.term;
 		$scope.currentPage = 1;
+		$scope.Math = window.Math;
 		let defaultZipCode = 60661;
 
 		if ($scope.searchTerm) {
@@ -19,7 +20,6 @@
 
 		function getCurrentWeather(zip) {
 			customSearch.getCurrentWeather(zip).then(function(result){
-				console.log(result);
 				$scope.location = result;
 			});
 		}
--- services/searchService.js
+++ services/searchService.js
@@ -1,22 +1,29 @@
 angular.module("searchApp")
 .factory('customSearch', function($http) { 
 	let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+	let forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 	let appId = '40618c713c32e058feb5d290739f141c';
 
-    let getCurrentWeather = function(searchTerm) {
-	    return $http.get(`${weatherUrl}?zip=${searchTerm},us&units=imperial&appid=${appId}`).then(function(result){
+    let getCurrentWeather = function(zip) {
+	    return $http.get(`${weatherUrl}?zip=${zip},us&units=imperial&appid=${appId}`).then(function(result){
+			return $http.get(`${forecastUrl}?zip=${zip},us&units=imperial&appid=${appId}`).then(function(forecast) {
+				result.data.forecast = forecast.data.list.filter(result => result.dt_txt.includes('00:00:00'));
 				return result.data;
+			})
 	    });
 	};
 
 	let getGeoWeather = function(coords) {
 		return $http.get(`${weatherUrl}?lat=${coords.latitude}&lon=${coords.longitude}&units=imperial&appid=${appId}`).then(function(result){
-			return result.data;
+			return $http.get(`${forecastUrl}?lat=${coords.latitude}&lon=${coords.longitude}&units=imperial&appid=${appId}`).then(function(forecast) {
+				result.data.forecast = forecast.data.list.filter(result => result.dt_txt.includes('00:00:00'));
+				return result.data;
+			})
 		});
 	}
 
     return {
-			getCurrentWeather,
-			getGeoWeather
+		getCurrentWeather,
+		getGeoWeather
     }  
 });
